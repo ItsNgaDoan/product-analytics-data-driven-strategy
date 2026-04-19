ba1627424e43275cd4746f17fd9e95f9eff7378a
ItsNgaDoan <doannga3012@gmail.com>
2026-04-19 23:08:43 +1200

    Update product-configuration.sql

--- sql/product-configuration.sql
+++ sql/product-configuration.sql
@@ -1,16 +1,16 @@
 -- Analysis: Product configuration
--- Business Question: How do different product configurations (features, color, size, and style) impact sales performance and customer preferences?
-
--- SQL STATEMENT:
-SELECT
-    dp.CategoryName,
-    dp.ProductLine,
-    dp.Style,
-    dp.Color,
-    sum(fp.OrderQty) AS TotalSalesCount,
-    SUM(fp.ActualSaleValue) AS TotalSales,
-    sum(fp.ProfitMargin) AS totalProfitMargin
-FROM FactProductPerformance fp
-JOIN DimProduct dp ON fp.ProductID = dp.ProductID
-GROUP BY dp.CategoryName,dp.ProductLine, dp.Style, dp.Color
-ORDER BY totalProfitMargin DESC, dp.CategoryName, dp.Style;
+    -- Business Question: How do different product configurations (features, color, size, and style) impact sales performance and customer preferences?
+    
+            -- SQL statement
+            SELECT
+                dp.CategoryName,
+                dp.ProductLine,
+                dp.Style,
+                dp.Color,
+                sum(fp.OrderQty) AS TotalSalesCount,
+                SUM(fp.ActualSaleValue) AS TotalSales,
+                sum(fp.ProfitMargin) AS totalProfitMargin
+            FROM FactProductPerformance fp
+            JOIN DimProduct dp ON fp.ProductID = dp.ProductID
+            GROUP BY dp.CategoryName,dp.ProductLine, dp.Style, dp.Color
+            ORDER BY totalProfitMargin DESC, dp.CategoryName, dp.Style;
